2111bf0cbbcb019197413424baa5bcb97302f1bd
suman <suman143learning@gmail.com>
2020-09-15 08:45:36 +0530

    changes done as per review comments

--- project4/src/test/java/tests/TestSuite2.java
+++ project4/src/test/java/tests/TestSuite2.java
@@ -7,13 +7,10 @@ import org.junit.Test;
 public class TestSuite2 {
 
 	@Test
-	public void test1() {
-		System.out.println("test1");
-	}
-	@Test
 	public void test2() {
 		System.out.println("test2");
 	}
+	//test case 3
 	@Test
 	public void test3() {
 		System.out.println("test3");
